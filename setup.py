282bd920e750ad3980ea7f0d4e7b74e342399bef
lpmatos <luccapessoamatos@gmail.com>
2020-07-27 11:25:42 -0300

    feat: dockerfile, docker-compose and others

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='gitlab-clone',
+  version='1.0',
+  description='Command line tool to clone projects inside groups in GitLab',
+  url='https://github.com/lpmatos/gitlab-clone',
+  author='Lucca Pessoa da Silva Matos',
+  license='MIT',
+  packages=['gitlab-clone'],
+  zip_safe=False,
+  install_requires=[
+    'click',
+    'requests',
+    'colorama'
+  ],
+  entry_points = {
+    'console_scripts': ['gitlab-clone=gitlab-clone.cli:main'],
+  }
+)
