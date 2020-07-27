229df6a8aa20900bffc572dbe98b269da20af49d
lpmatos <luccapessoamatos@gmail.com>
2020-07-27 16:44:00 -0300

    fix: setup file changes

--- setup.py
+++ setup.py
@@ -1,27 +1,52 @@
 # -*- coding: utf-8 -*-
 
+import io
+import os
 from cloner import __version__
 from setuptools import setup, find_packages
 
-requirements = [
+# Package meta-data.
+NAME              = "gitlab-cr"
+DESCRIPTION       = "CLI to help you clone projects inside groups in Gitlab"
+URL               = "https://github.com/lpmatos/gitlab-clone-recursive"
+EMAIL             = "luccapsm@gmail.com"
+AUTHOR            = "Lucca Pessoa da Silva Matos"
+REQUIRES_PYTHON   = ">=3.6.0"
+VERSION           = __version__
+
+# What packages are required for this module to be executed?
+REQUIRED = [
   "python-gitlab"
 ]
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+  with io.open(os.path.join(here, "README.md"), encoding="utf-8") as file:
+    LONG_DESCRIPTION = "\n" + file.read()
+except FileNotFoundError:
+  LONG_DESCRIPTION = DESCRIPTION
+
 setup(
-  author = "Lucca Pessoa da Silva Matos",
-  author_email = "luccapsm@gmail.com",
-  description = "Gitlab tool for recursive clone",
-  install_requires = requirements,
-  license = "MIT license",
+  name = NAME,
+  version = VERSION,
+  description = DESCRIPTION,
+  long_description = LONG_DESCRIPTION,
+  long_description_content_type = "text/markdown",
+  author = AUTHOR,
+  author_email = EMAIL,
+  python_requires = REQUIRES_PYTHON,
+  url=URL,
+  packages = find_packages(include=["cloner"], exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+  install_requires = REQUIRED,
   include_package_data = True,
+  license = "MIT license",
   keywords = [
     "gitlab",
     "gitlab-api",
   ],
-  name = "gitlab-cr",
-  packages = find_packages(include=["cloner"]),
-  url = "https://github.com/lpmatos/gitlab-clone-recursive",
-  version = __version__,
   entry_points = {
     "console_scripts" : [
       "gitlab-cr=cloner.cli:main",
